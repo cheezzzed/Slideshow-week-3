ff8b23ec27f97065751dd40cc446b0299316a1b2
Johnny Sanchez <johnny062999@gmail.com>
2022-01-26 18:14:54 -0500

    Slideshow-transition-update

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,42 @@
-// new rule: no unintended consequences.
-// don't fuck with anything else on the page.
-// if we have more than one slideshow, they should run independently.
-// that means SCOPE! both in variables and in finding elements
-// for the variable scope we're actually going to learn a whole new technique, called Functional Prototypes
-// this is where you write a function that builds a nice, self contained thing for you.
-// everything it needs is inside the function, and nothing is left outside, lying around, for other devs to metaphorically step on
-// if something is needed from outside, you can write your function to selectively expose certain things.
 
 
-let slides = document.querySelectorAll('.slideshow img')
-console.log(slides)
+let makeSlideshow = function(slideshowParentSelector){
+    let slides = document.querySelectorAll(slideshowParentSelector + ' img')
 
-slides[0].classList.add('show')
+    slides[0].classList.add('show')
 
-let slideIndex = 0
-setInterval(function() {
+    let slideIndex = 0
 
-	slides[slideIndex].classList.remove('show')
-	slideIndex++
-	if (slideIndex === slides.length) {
-		slideIndex = 0
-	}
-	slides[slideIndex].classList.add('show')
+    let doSlideTransition = function() {
+        slides[slideIndex].classList.remove('show')
+        slideIndex++
+        if (slideIndex === slides.length){
+            slideIndex = 0
+        }
+        slides[slideIndex].classList.add('show')
+    }
 
+    let doSlideTransitionBack = function() {
+        slides[slideIndex].classList.remove('show')
+        slideIndex -= 1
+        if (slideIndex === slides.length){
+            slideIndex = 0
+        }
+        slides[slideIndex].classList.add('show')
+    }
 
+    let next = document.querySelector('.next')
 
+    next.addEventListener('click' , function(){
+        doSlideTransition()
+    }
 
-}, 3000)
+    let prev = document.querySelector('.previous')
+
+    prev.addEventListener('click' , function(){
+        doSlideTransitionBack()
+    }
+
+}
+
+makeSlideshow(".slideshow")
